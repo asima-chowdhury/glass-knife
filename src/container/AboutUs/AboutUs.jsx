3d91eecdaaf72b399757cb73d4f1d2a2276bf4d0
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-02-28 22:17:20 +0600

    about completed with responsive

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -9,7 +9,8 @@ const AboutUs = () => (
    <div className="app__aboutus-overlay flex__center">
      <img src={images.G} alt="G-letter" />
    </div>
-   <div className="app__aboutus-content flex-center">
+
+   <div className="app__aboutus-content flex__center">
      <div className="app__aboutus-content_about">
        <h1 className="headtext__cormorant">About US</h1>
        <img src={images.spoon} alt="about_spoon" className='spoon__img' />
@@ -17,7 +18,7 @@ const AboutUs = () => (
        <button type='button' className='custom__button'>Know More</button>
      </div>
 
-     <div className="app__aboutus-content_knife">
+     <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="knife" />
      </div>
 
