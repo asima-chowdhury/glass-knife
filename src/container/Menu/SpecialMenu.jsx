695fc039db8b72a4cc848cb9dce7f1771befdf26
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-03-01 22:44:29 +0600

    in progress specialMenu part

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -9,7 +9,7 @@ const SpecialMenu = () => (
   <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className="app__specialMenu-title">
       <SubHeading title="Menu that fits you palatte"/>
-
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
   </div>
 );
