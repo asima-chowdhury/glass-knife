94fbdfbfdd48e41ba9f846f3815dc79a97fa7ed4
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-03-02 23:54:13 +0600

    in progress specialMenu part

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-import {SubHeading, MenuItem} from '../../components';
-import {images, data} from '../../constants';
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
   <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits you palatte"/>
+      <SubHeading title="Menu that fits you palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu_img">
+      <img src={images.menu} alt="menu__img" />
+    </div>
+
+    <div style={{ marginTop: 15 }}>
+      <button type="button" className="custom__button">View More</button>
     </div>
   </div>
 );
